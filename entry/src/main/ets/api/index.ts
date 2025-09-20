1acb59afca585ce613fee32e68d8fa984448f6a5
wuyuanwuhui99 <275018723@qq.com>
2025-09-20 15:30:51 +0800

    修改租户管理问题

--- entry/src/main/ets/api/index.ts
+++ entry/src/main/ets/api/index.ts
@@ -27,5 +27,6 @@ export default {
   getTenantUser: "/service/tenant/getTenantUser",// 获取当前租户下当前用户角色信息
   addTenantUser:"/service/tenant/addTenantUser/",// 添加用户
   addAdmin:"/service/tenant/addAdmin",// 设置为管理员
-  deleteAdmin:"/service/tenant/deleteAdmin"// 取消管理员
+  deleteAdmin:"/service/tenant/deleteAdmin",// 取消管理员
+  deleteTenantUser: "/service/tenant/deleteTenantUser",// 删除用户
 };
